072a267099e6601c982b9fd6b7ba981b8ca11239
artarts36 <temicska99@mail.ru>
2023-02-13 01:41:50 +0300

    add custom entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+set -e
+
+MR_LINTER_DEBUG=""
+if [ "$INPUT_DEBUG" = "true" ]; then
+    MR_LINTER_DEBUG="--debug"
+fi
+
+MR_LINTER_METRICS=""
+if [ "$INPUT_METRICS" = "true" ]; then
+    MR_LINTER_METRICS="--metrics"
+fi
+
+MR_LINTER_CONFIG=""
+if [ ! -z "$INPUT_CONFIG" ]; then
+    MR_LINTER_CONFIG="--config=$INPUT_CONFIG"
+fi
+
+/app/bin/mr-linter --version
+/app/bin/mr-linter lint $MR_LINTER_DEBUG $MR_LINTER_METRICS "$MR_LINTER_CONFIG"
